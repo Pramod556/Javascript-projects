2f3aa9664e75985a276546310cc60e83d9e829c6
Pramod Swargam <98465143+Pramod556@users.noreply.github.com>
2023-06-06 15:00:37 +0530

    first=project

---
+++ color-flipper/hex.js
@@ -0,0 +1,17 @@
+const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+
+
+const btn = document.getElementById('btn')
+const color = document.querySelector('.color')
+
+btn.addEventListener('click', function () {
+    let hexColor = "#"
+   for (let index = 0; index <6; index++) {
+    hexColor =hexColor+hex[Math.floor(Math.random()*hex.length)];
+    
+   }
+    console.log(hexColor);
+    document.body.style.backgroundColor = hexColor
+    color.style.color = hexColor
+    color.textContent=hexColor
+})
